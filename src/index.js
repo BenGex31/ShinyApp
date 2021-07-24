13306ec0a9d8dd383da7fc9571f02c6dfe7ea772
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-07-25 00:20:20 +0200

    Delete App.jsx Include App.jsx in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Survey from "./pages/Survey";
+import Results from "./pages/Results";
+import Freelances from "./pages/Freelances";
+import Header from "./components/Header";
+import Error from "./components/Error";
+import { createGlobalStyle } from "styled-components";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
+    }
+`;
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+  <React.StrictMode>
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+        <Route path="/freelances">
+          <Freelances />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
