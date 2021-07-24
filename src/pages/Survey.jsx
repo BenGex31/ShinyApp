224a952ace765d159c3c32d49f5bb8b3254fff35
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-07-24 23:00:32 +0200

    Udpate Survey

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
+import colors from "../utils/style/colors";
+import { Loader } from "../utils/style/Atom";
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`;
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`;
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`;
 
 function Survey() {
   const { questionNumber } = useParams();
@@ -7,18 +35,48 @@ function Survey() {
   const prevQuestionNumber =
     questionNumberInt === 1 ? 1 : questionNumberInt - 1;
   const nextQuestionNumber = questionNumberInt + 1;
+  const [surveyData, setSurveyData] = useState({});
+  const [isDataLoading, setDataLoading] = useState(false);
+
+  /*async function fetchData() {
+    try {
+      const response = await fetch(`http://localhost:8000/survey`);
+      const { surveyData } = await response.json();
+      setSurveyData(surveyData);
+    } catch (error) {
+      console.log("===== error =====", error);
+      setError(true);
+    }
+  }*/
+
+  useEffect(() => {
+    //fetchData();
+    setDataLoading(true);
+    fetch(`http://localhost:8000/survey`).then((response) =>
+      response.json().then(({ surveyData }) => {
+        setSurveyData(surveyData);
+        setDataLoading(false);
+      })
+    );
+  }, []);
 
   return (
-    <div>
-      <h1>Questionnaire</h1>
-      <h2>Question n° {questionNumber} </h2>
-      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-      {questionNumberInt === 10 ? (
-        <Link to="/results">Résultats</Link>
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
       ) : (
-        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
       )}
-    </div>
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   );
 }
 
