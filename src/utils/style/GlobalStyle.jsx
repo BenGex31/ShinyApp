0573faee94c9c6037c9bd92a511ccc9a423107b6
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-07-25 01:08:05 +0200

    Update Switch dark mode

--- src/utils/style/GlobalStyle.jsx
+++ src/utils/style/GlobalStyle.jsx
@@ -1,21 +1,21 @@
-import { createGlobalStyle } from "styled-components";
 import { useContext } from "react";
-import { ThemeContext } from "../context/index";
+import { createGlobalStyle } from "styled-components";
+import { ThemeContext } from "../context";
 
 const StyledGlobalStyle = createGlobalStyle`
     * {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
     }
- 
     body {
         background-color: ${({ isDarkMode }) =>
-          isDarkMode ? "black" : "white"};
-        margin: 0;  
+          isDarkMode ? "#2F2E41" : "white"};
+        margin: 0;
     }
 `;
 
-export default function GlobalStyle() {
-  const theme = useContext(ThemeContext);
-
+function GlobalStyle() {
+  const { theme } = useContext(ThemeContext);
   return <StyledGlobalStyle isDarkMode={theme === "dark"} />;
 }
+
+export default GlobalStyle;
