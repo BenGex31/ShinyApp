ea3b876974a1bce5ebc9c7ebc8d1b80469be247e
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-06 14:27:58 +0200

    Update results.test.js
    test formatFetchParams function

--- src/pages/results.test.js
+++ src/pages/results.test.js
@@ -1,4 +1,4 @@
-import { formatJobList } from "./Results";
+import { formatJobList, formatFetchParams } from "./Results";
 
 describe("The formatJobList function", () => {
   it("should add a comma to a word", () => {
@@ -9,5 +9,19 @@ describe("The formatJobList function", () => {
   it("should not add a comma to the last element of the list", () => {
     const expectedState = "item3";
     expect(formatJobList("item3", 3, 2)).toEqual(expectedState);
+  });
+});
+
+describe("The formatFetchParams function", () => {
+  it("should use the right format for param", () => {
+    const expectedState = "a1=answer1";
+    expect(formatFetchParams({ 1: "answer1" })).toEqual(expectedState);
+  });
+
+  it("should concatenate params with an &", () => {
+    const expectedState = "a1=answer1&a2=answer2";
+    expect(formatFetchParams({ 1: "answer1", 2: "answer2" })).toEqual(
+      expectedState
+    );
   });
 });
