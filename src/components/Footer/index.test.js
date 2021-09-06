8538df41b414dbfc736504b92c5df0bf0e8d39be
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-06 15:04:56 +0200

    Update index.test.js

--- src/components/Footer/index.test.js
+++ src/components/Footer/index.test.js
@@ -1,5 +1,5 @@
 import Footer from "./index";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { ThemeProvider } from "../../utils/context";
 
 describe("Footer", () => {
@@ -9,5 +9,16 @@ describe("Footer", () => {
         <Footer />
       </ThemeProvider>
     );
+  });
+  test("should change theme", async () => {
+    render(
+      <ThemeProvider>
+        <Footer />
+      </ThemeProvider>
+    );
+    const nightModeButton = screen.getByRole("button");
+    expect(nightModeButton.textContent).toBe("Changer de mode : ☀️");
+    fireEvent.click(nightModeButton);
+    expect(nightModeButton.textContent).toBe("Changer de mode : 🌙");
   });
 });
